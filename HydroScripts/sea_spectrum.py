494ffeb3f2f9d111a1ce2fcd0dfd965b34ce5aa4
Daniel Prata Vieira <dpratavieira@gmail.com>
2019-12-12 14:50:50 -0300

    plot seaspectrum

--- HydroScripts/sea_spectrum.py
+++ HydroScripts/sea_spectrum.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import style
 
-
 def converte_np_array(x):
     if isinstance(x, list):
         a = np.array(x)
@@ -11,7 +10,6 @@ def converte_np_array(x):
     else:
         a = x
     return a
-
 
 def seaspec(formulation, tp, hs, depth=100):
     g = 9.80665
@@ -45,47 +43,43 @@ def seaspec(formulation, tp, hs, depth=100):
         sw_aux = (2*pi)**-1*(5/16*hs**2*tp*((2*pi/tp)/w)**5*(1-0.287*np.log(gama))*np.exp(-1.25*(w/(2*pi/tp))**-4)*gama**np.exp(-((w-(2*pi/tp))/2/pi)**2/(2*((w<=(2*pi/tp))*0.07+(w>(2*pi/tp))*0.09)**2*((2*pi/tp)/2/pi)**2)))
         sw = sw_aux * coef_tma
 
-        m0_aux = np.trapz(sw_aux, x=w, axis=0)
+        m0_j2 = np.trapz(sw_aux, x=w, axis=0)
         m0_tma = np.trapz(sw, x=w, axis=0)
 
-        sw2 = sw * (m0_aux / m0_tma)
+        sw2 = sw * (m0_j2 / m0_tma)
 
-        # plt.plot(w, sw_aux, 'b')
-        # plt.plot(w, sw, 'r')
-        # plt.plot(w, sw2, 'k')
-        # plt.show(block = False)
         return sw2, w
 
+def plot_spectrum(w, Sw, Hs, Tp):
+    style.use('tableau-colorblind10')
+    # ['bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-bright',
+    # 'seaborn-colorblind', 'seaborn-dark-palette', 'seaborn-dark', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted',
+    # 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks',
+    # 'seaborn-white', 'seaborn-whitegrid', 'seaborn', 'Solarize_Light2', 'tableau-colorblind10', '_classic_test']
+    Hs_check = 4 * np.sqrt(np.trapz(Sw, x = w, axis=0))
+    tx = legenda(Hs_check)
+    Tp = converte_np_array(Tp)
+    plt.plot(2*np.pi/w, Sw)
+    plt.xlim(0, 2*Tp.max())
+    plt.xlabel('Tp [s]')
+    plt.ylabel('S [m²s]')
+    plt.grid(True)
+    plt.legend(tx)
+    plt.show()
 
 def legenda(hs):
     hs = converte_np_array(hs)
     texto = []
     i = 0
     while i < len(hs):
-        texto.append('Hs = %.3f m' % hs[i])
+        texto.append('Hs = %.2f m' % hs[i])
         i += 1
     return texto
 
-
-style.use('tableau-colorblind10')
-# ['bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-bright',
-# 'seaborn-colorblind', 'seaborn-dark-palette', 'seaborn-dark', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted',
-# 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks',
-# 'seaborn-white', 'seaborn-whitegrid', 'seaborn', 'Solarize_Light2', 'tableau-colorblind10', '_classic_test']
-
+# Debug
 # Tp = np.array([10, 10, 10])
 # Hs = np.array([2, 4, 3])
-Tp = [10, 20]
-Hs = [3, 2]
-Sw1, w1 = seaspec('tma', Tp, Hs, 40)
-
-Hs_check = 4 * np.sqrt(np.trapz(Sw1, x = w1, axis=0))
-
-# print(Hs_check)
-tx = legenda(Hs_check)
-Tp = converte_np_array(Tp)
-plt.plot(2*np.pi/w1, Sw1)
-plt.xlim(0, 2*Tp.max())
-plt.grid(True)
-plt.legend(tx)
-plt.show()
+Tp = [8, 10, 12, 14]
+Hs = [4, 3,  2,  1]
+Sw1, w1 = seaspec('j2', Tp, Hs, 40)
+plot_spectrum(w1, Sw1, Hs, Tp)
