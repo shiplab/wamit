10e5f32a127843889d526869260137070a4a5ed2
Daniel Prata Vieira <dpratavieira@gmail.com>
2020-01-30 19:43:47 -0300

    write_wnf_tpn

---
+++ HydroScripts/write_wnf_tpn.py
@@ -0,0 +1,273 @@
+import numpy as np
+import analysis_wamit
+import sys
+
+if len(sys.argv)>1:
+    dt = sys.argv[1]
+else:
+    dt = ''
+
+val=dt
+
+while (val != 'm') & (val != 'p') & (val != 'c'):
+    val = input('Drift Analysis: [''m'',''p'',''c'']: ')
+    print(val != 'm')
+    print(val != 'p')
+    print(val != 'c')
+    
+dt = val
+
+
+# Reading Output parameters
+
+p_out = analysis_wamit.output_params()
+
+[params,axis,vol,cb,cg,rest_coef,name_out,GM,GMl] = p_out
+
+[g,ulen, rho, water_depth, water_depth_aux, NBODY] = params
+
+# Reading RAO
+[rao, rao_phase, per, inc, dof, arq4d, rao_c] = analysis_wamit.raos(param_out=p_out)
+
+# Reading Wave Forces
+[wforce, wforce_phase, arq2d] = analysis_wamit.wave_forces(param_out=p_out)
+    
+# Reading Drift forces ('m' - Momentum, 'p' - Pressure, 'c' - control surface)
+
+
+[wdforce, wdforce_phase, arq8d] = analysis_wamit.drift_forces(drift_analysis_type = dt, param_out=p_out)
+
+# Reading Added Mass and Potential Damping
+[added_mass, pot_damp, dof1, arq1d, added_mass_matrix, pot_damp_matrix]= analysis_wamit.added_mass_pot_damping(param_out=p_out)
+
+dof=np.reshape(dof,(NBODY,6))
+
+
+for ii in range(NBODY):
+    [xbody,ybody,zbody,phibody] = axis[ii]
+    [xvol,yvol,zvol] = vol[ii]
+    [xb,yb,zb] = cb[ii]
+    [xg,yg,zg] = cg[ii]
+    [c33,c34,c35,c44,c45,c46,c55,c56] = rest_coef[ii]
+
+    # Reading Force file 
+    [mass,damp,rest_coef_ext] = analysis_wamit.read_frc()
+    
+    # Names of Degrees of Freedom
+    naemsDof_aux = ['SURGE','SWAY','HEAVE','ROLL','PITCH','YAW']
+    namesDof=[]
+    for nn in range(NBODY):
+        for nd in naemsDof_aux:
+            namesDof.append(nd)
+        
+    # Write parameters in ship.wnf
+    name_wnf = 'ship{:d}.wnf'.format(ii + 1)
+    wnf = open(name_wnf,'w')
+    wnf.write('%VERSION\n')
+    wnf.write('2.1.4\n\n')
+    wnf.write('%NUMBER_OF_BODIES\n')
+    wnf.write(str(NBODY) + '\n\n')
+    wnf.write('%BODY\n')
+    wnf.write(str(ii+1) + '\n\n')
+    wnf.write('%SCALE\n')
+    wnf.write(str(ulen)+'\n\n')
+    wnf.write('%WATER_DENSITY\n')
+    wnf.write(str(rho)+'\n\n')
+    wnf.write('%GRAVITY\n')
+    wnf.write(str(g)+'\n\n')
+    wnf.write('%WAMIT_AXIS\n')
+    wnf.write('{:.4f}'.format(xbody)+' '+'{:.4f}'.format(ybody)+' '+'{:.4f}'.format(zbody)+' '+'{:.1f}'.format(phibody)+'\n\n')
+    wnf.write('%PHIBODY\n')
+    wnf.write('{:.1f}'.format(phibody)+'\n\n') 
+    wnf.write('%SYMMETRY\n')
+    wnf.write('0\n\n')
+    wnf.write('%CENTER_OF_BUOYANCY\n')
+    wnf.write('{:.6f}'.format(xb)+' '+'{:.6f}'.format(yb)+' '+'{:.6f}'.format(zb)+'\n\n')
+    wnf.write('%CENTER_OF_GRAVITY\n')
+    wnf.write('{:.6f}'.format(xg)+' '+'{:.6f}'.format(yg)+' '+'{:.6f}'.format(zg)+'\n\n')
+    wnf.write('%DEPTH\n')
+    # wnf.write('\n\n')
+    if water_depth_aux==1:
+        wnf.write(water_depth+'\n\n')
+    else:
+        wnf.write('{:f}'.format(water_depth)+'\n\n')
+
+    wnf.write('%VOLUME\n')
+    wnf.write('{:.0f}'.format(np.mean([xvol,yvol,zvol]))+'\n\n') #mean volume
+    
+    wnf.write('%VOLUMEX\n')
+    wnf.write('{:.0f}'.format(xvol)+'\n\n')
+    wnf.write('%VOLUMEY\n')
+    wnf.write('{:.0f}'.format(yvol)+'\n\n')
+    wnf.write('%VOLUMEZ\n')
+    wnf.write('{:.0f}'.format(zvol)+'\n\n')
+
+    # wnf.write('%DISPLACEMENT_WEIGHT\n')
+    # wnf.write('{:.1f}'.format(mass[ii][0][0])+'\n\n')
+    
+    wnf.write('%MASS_AND_INERTIA\n')
+    for i in mass[ii]:
+        wnf.write('    '.join('{:.5e}'.format(x) for x in i)+'\n')
+        
+    wnf.write('\n')
+
+    wnf.write('%EXTERNAL_GLOBAL_DAMPING\n')
+    for i in damp[ii]:
+        wnf.write('    '.join('{:.5e}'.format(x) for x in i)+'\n')
+        
+    wnf.write('\n')
+    wnf.write('%NUMBER_FREQUENCIES\n')
+    wnf.write(str(per.size)+'\n')
+    wnf.write('%NUMBER_HEADINGS\n')
+    wnf.write(str(inc.size)+'\n')
+    tx_inc = 12*' '
+    
+    for x in inc:
+        n_dec = 0
+        z = x % 1
+        while z != 0:
+            n_dec = n_dec + 1
+            z = z*10 % 1
+            # print(n_dec)
+            # print(z)
+
+        aux = '{:.' + '{:d}'.format(n_dec) +'f}'
+        # print(aux)
+        aux = aux.format(x)
+        n_carac = len(aux)
+        tx_inc = tx_inc + (16-n_carac)*' ' + aux
+        
+    #RAOs
+    for j in range(dof[ii].size):
+        wnf.write('%RAO_' + namesDof[j] + '_MOD\n')
+        wnf.write(tx_inc + '\n')
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in rao[ int(dof[ii][j])-1 ][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+        wnf.write('\n')
+        wnf.write('%RAO_' + namesDof[j] + '_PHA\n')
+        wnf.write(tx_inc + '\n')
+        
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in rao_phase[j][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+        wnf.write('\n')
+    
+    #EXCITING_WAVE_FORCE
+    for j in range(dof[ii].size):
+        wnf.write('%HASKIND_EXCITING_FORCES_' + namesDof[j] + '_MOD\n')
+        wnf.write(tx_inc + '\n')
+        
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in wforce[int(dof[ii][j])-1][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+        wnf.write('\n')
+        wnf.write('%HASKIND_EXCITING_FORCES_' + namesDof[j] + '_PHA\n')
+        wnf.write(tx_inc + '\n')
+        
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in wforce_phase[j][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+        wnf.write('\n')
+    
+    #SLOW_DRIFT_FORCE
+    for j in range(dof[ii].size):
+        wnf.write('%DRIFT_FORCES_' + namesDof[j] + '_MOD\n')
+        wnf.write(tx_inc + '\n')
+        
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in wdforce[int(dof[ii][j])-1][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+            
+        wnf.write('\n')
+        wnf.write('%DRIFT_FORCES_' + namesDof[j] + '_PHA\n')
+        wnf.write(tx_inc + '\n')
+        
+        for i in range(per.size):
+            tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+            for x in wdforce_phase[j][i]:
+                aux = '{:.5e}'.format(x)
+                n_carac = len(aux) 
+                tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+            wnf.write(tx_rao + '\n')
+        wnf.write('\n')
+        
+    #ADDED_MASS
+    tx_ad = 12*' '
+    sz = int(dof1.shape[0]/NBODY)
+    dof1_aux = dof1[ii*sz:ii*sz+sz]
+    
+    for x in dof1_aux:
+        str_aux = 'A(' + '{:d}'.format(int(x[0])) + ',' '{:d}'.format(int(x[1])) + ')'
+        blank_spaces = 16 - len(str_aux)
+        tx_ad = tx_ad + blank_spaces*' ' + str_aux
+    wnf.write('\n')
+    wnf.write('%ADDED_MASS\n')
+    wnf.write(tx_ad + '\n')
+    
+    for i in range(per.size):
+        tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+        for x in added_mass[i][ii*sz:ii*sz+sz]:
+            aux = '{:.5e}'.format(x)
+            n_carac = len(aux) 
+            tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+        wnf.write(tx_rao + '\n')
+        
+    #POTENTIAL_DAMPING
+    tx_pd = 12*' '
+    
+    for x in dof1_aux:
+        str_aux = 'B(' + '{:d}'.format(int(x[0])) + ',' '{:d}'.format(int(x[1])) + ')'
+        blank_spaces = 16 - len(str_aux)
+        tx_pd = tx_pd + blank_spaces*' ' + str_aux
+    
+    wnf.write('\n')
+    wnf.write('%POTENTIAL_DAMPING\n')
+    wnf.write(tx_pd + '\n')
+    
+    for i in range(per.size):
+        tx_rao = '{:.5e}'.format(2*np.pi/per[i])
+        for x in pot_damp[i][ii*sz:ii*sz+sz]:
+            aux = '{:.5e}'.format(x)
+            n_carac = len(aux) 
+            tx_rao = tx_rao + (16-n_carac)*' ' + aux 
+        wnf.write(tx_rao + '\n')
+    
+    #REST_Coefs
+    wnf.write('\n')
+    wnf.write('%HYDROSTATIC_RESTORING\n')
+    wnf.write('{:.5e}'.format(rest_coef[ii][0]))
+    
+    for x in rest_coef[ii][1:]:
+        aux = '{:.5e}'.format(x)
+        n_carac = len(aux)
+        wnf.write((16-n_carac)*' ' + aux)
+        
+    wnf.write('\n')
+    wnf.write('\n')
+    wnf.write('%END\n')    
+    wnf.close()
+
+print('')
+print(' * Generated WNF file(s)' )
+print('')
